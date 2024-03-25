7b3c08ddd01ec8bc93eb8746453ef0ee8f696396
Kabir-iqbal <161919126+Kabir-Iqbal@users.noreply.github.com>
2024-03-25 19:51:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// 9. Favorite Number: 
+// Store your favorite number in a variable. Then, using that variable, create a message that 
+// reveals your favorite number. Print that message.
+var favoritenum = 9;
+console.log("My Favourite name :".concat(favoritenum));
